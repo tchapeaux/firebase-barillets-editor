c35b92ed4310481d597a60d87158e0cd61f84c2c
Thomas Chapeaux <tom@wooclap.com>
2025-10-23 17:41:52 +0200

    fix: missing portal in dropdown menu

--- src/components/ui/dropdownMenu.vue
+++ src/components/ui/dropdownMenu.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {
   DropdownMenuContent,
+  DropdownMenuPortal,
   DropdownMenuRoot,
   DropdownMenuTrigger,
 } from 'radix-vue';
@@ -21,15 +22,17 @@ const props = withDefaults(defineProps<Props>(), {
       <slot name="trigger" />
     </DropdownMenuTrigger>
 
-    <DropdownMenuContent
-      :align="props.align"
-      :class="
-        cn(
-          'z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2'
-        )
-      "
-    >
-      <slot />
-    </DropdownMenuContent>
+    <DropdownMenuPortal>
+      <DropdownMenuContent
+        :align="props.align"
+        :class="
+          cn(
+            'z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2'
+          )
+        "
+      >
+        <slot />
+      </DropdownMenuContent>
+    </DropdownMenuPortal>
   </DropdownMenuRoot>
 </template>
